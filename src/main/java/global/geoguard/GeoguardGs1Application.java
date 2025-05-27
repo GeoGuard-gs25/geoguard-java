ea81685b37683f5398cdc07853b2f7987585ac2e
Guilherme Alves Pedroso <alvespedrosog@gmail.com>
2025-05-26 22:02:50 -0300

    Fistr commit

---
+++ src/main/java/global/geoguard/GeoguardGs1Application.java
@@ -0,0 +1,13 @@
+package global.geoguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoguardGs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoguardGs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/global/geoguard/GeoguardGs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package global.geoguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoguardGs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
